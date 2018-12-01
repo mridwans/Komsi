203860d0cdeced09b16dc79553c15bb479e6d717
Putri Mangifera Indika <putrimangifera@gmail.com>
2018-12-01 22:56:42 +0700

    first commit

--- app/src/main/java/com/example/user/komsi/MainActivity.java
+++ app/src/main/java/com/example/user/komsi/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.user.komsi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -42,8 +43,10 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "HAHAHAHHAA...", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(getApplicationContext(),TambahPenggunaActivity.class);
+                startActivity(intent);
+                /*Snackbar.make(view, "HAHAHAHHAA...", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
             }
         });
 
---
+++ app/src/main/java/com/example/user/komsi/TambahPenggunaActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.komsi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TambahPenggunaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah_pengguna);
+    }
+}
