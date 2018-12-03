a190edd5013bba5d4c14815db566a5fea6f49888
M Ridwan Shalahuddin <m.r.shalahuddin@gmail.com>
2018-12-03 16:08:57 +0700

    yolo

--- app/src/main/java/com/example/user/komsi/MainActivity.java
+++ app/src/main/java/com/example/user/komsi/MainActivity.java
@@ -3,35 +3,24 @@ package com.example.user.komsi;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-<<<<<<< HEAD
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
-=======
->>>>>>> origin/ridwan
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-<<<<<<< HEAD
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-=======
-import android.view.View;
->>>>>>> origin/ridwan
 
 import java.util.ArrayList;
-
 
 //private final LinkedList<String> mWordList = new LinkedList<>();
 //private int mCount = 0;
@@ -71,6 +60,8 @@ public class MainActivity extends AppCompatActivity
                         if(!mNamaFolder.getText().toString().isEmpty()){
                             Toast.makeText(MainActivity.this,
                                     R.string.sukses_msg, Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(MainActivity.this,AddEventActivity.class);
+                            startActivity(intent);
                         }else {
                             Toast.makeText(MainActivity.this,
                                     R.string.kosong_msg, Toast.LENGTH_SHORT).show();
@@ -169,8 +160,7 @@ public class MainActivity extends AppCompatActivity
             startActivity(intent);
 
         } else if (id == R.id.nav_search) {
-            Intent intent = new Intent(MainActivity.this,DateRangePickerActivity.class);
-            startActivity(intent);
+
         } else if (id == R.id.nav_logout) {
 
         }
